5493e827e5389980dc74f3ead541d012709b8082
Ubuntu <shiyu_k1994@qq.com>
2020-10-15 12:25:42 +0000

    add tuning dataset selection

--- tune_all.sh
+++ tune_all.sh
@@ -1,14 +1,16 @@
+#!/bin/bash
+
 cd tune/lightgbm
-python gen_bach.py
+python gen_bach.py $1 $2
 ./tune.sh
 cd ../..
 
 cd tune/xgboost
-python gen_bach.py
+python gen_bach.py $1 $2
 ./tune.sh
 cd ../..
 
 cd tune/catboost
-python gen_bach.py
+python gen_bach.py $1 $2
 ./tune.sh
 cd ../..
